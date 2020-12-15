d3f3f23454aff9bcc17b18dd28cf527ff8d58393
mhekel9 <michael.hekel@daimler.com>
2020-12-15 16:28:14 +0100

    Restructured code, print out various data

--- Main.go
+++ Main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+
 	"github.com/goburrow/modbus"
 )
 
@@ -21,19 +22,14 @@ func main() {
 
 	client := modbus.NewClient(handler)
 
-	registers := []byte(außenluft)
-	registers = append(registers, 0x0, 0x0)
-
-	results, err := client.WriteMultipleRegisters(0, 4, registers)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to send value to Helios device: %v", err))
-	}
-	results, err = client.ReadHoldingRegisters(0, 6)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to receive value from Helios device: %v", err))
-	}
-
-	fmt.Println("außenluftTemp: ", getValueFromHeliosResponse(results))
+	fmt.Println("Außenluft:", getValueFromHelios(client, außenluft, 8))
+	fmt.Println("Zuluft:", getValueFromHelios(client, zuluft, 8))
+	fmt.Println("Fortluft:", getValueFromHelios(client, fortluft, 8))
+	fmt.Println("Abluft:", getValueFromHelios(client, abluft, 8))
+	fmt.Println()
+	fmt.Println("Zuluft RPM:", getValueFromHelios(client, zuluftRpm, 8))
+	fmt.Println("Abluft RPM:", getValueFromHelios(client, abluftRpm, 8))
+	fmt.Println("Lüfterstufe:", getValueFromHelios(client, lüfterstufe, 8))
 }
 
 const (
@@ -45,6 +41,22 @@ const (
 	zuluftRpm   = "v00348"
 	abluftRpm   = "v00349" // 0x76, 0x30, 0x30, 0x33, 0x34, 0x39, 0x00, 0x00
 )
+
+func getValueFromHelios(client modbus.Client, heliosVariable string, countBytesToRead byte) string {
+	registers := []byte(heliosVariable)
+	registers = append(registers, 0x0, 0x0)
+
+	results, err := client.WriteMultipleRegisters(0, 4, registers)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to send value to Helios device: %v", err))
+	}
+	results, err = client.ReadHoldingRegisters(0, 6)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to receive value from Helios device: %v", err))
+	}
+
+	return getValueFromHeliosResponse(results)
+}
 
 func getValueFromHeliosResponse(heliosResult []byte) (result string) {
 	return strings.SplitAfter(modbusResponseToString(heliosResult), "=")[1]
